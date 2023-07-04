9d9d06fe6630b396bf15c577550351a3c86867b5
TLara75 <138093190+TLara75@users.noreply.github.com>
2023-07-04 14:51:04 +0300

    Add files via upload

--- data_store.py
+++ data_store.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine, MetaData
 from config import DSN
 
 engine = create_engine(DSN)
-
 metadata = MetaData()
 Base = declarative_base()
 
@@ -12,11 +11,13 @@ class Diplom(Base):
     __tablename__ = 'diplom'
     profile_id = sq.Column(sq.Integer, primary_key=True)
     user_id = sq.Column(sq.Integer, primary_key=True)
+
 def add_user(engine, profile_id, user_id):
     with Session(engine) as session:
         to_bd = Diplom(profile_id=profile_id, user_id=user_id)
         session.add(to_bd)
         session.commit()
+
 def check_user(engine, profile_id, user_id):
     with Session(engine) as session:
         from_bd = session.query(Diplom).filter(
@@ -24,7 +25,6 @@ def check_user(engine, profile_id, user_id):
             Diplom.user_id == user_id
         ).all()
         return True if from_bd else False
-
 
 if __name__ == '__main__':
     engine = create_engine(DSN)
